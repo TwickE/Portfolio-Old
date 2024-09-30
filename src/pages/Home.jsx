55c56a9466a686c2236dc07232321576db26a899
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-09-30 22:13:33 +0100

    Added a background-color and a box-shadow when the user scrolls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
         <>
             <Navbar activeLink="home" />
             <h1>Home</h1>
+            <div style={{width: "300px", height: "3000px", backgroundColor: "red"}}></div>
         </>
         
     )
