9b2c8c43f89938088164ca0ac289419820b6f4eb
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-10-06 18:42:07 +0100

    Added a mouse light effect on the MySkillsSection

--- src/components/MySkillsSection.jsx
+++ src/components/MySkillsSection.jsx
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types';
 import SkillBadge from './sub-components/SkillBadge'
 import skillsJSON from '../assets/skills.json'
 import useLoadImages from '../hooks/useLoadImages'
+import { useRef } from 'react';
 
 function MySkillsSection(props) {
     const { skills, loadedImages } = useLoadImages(skillsJSON);
+    const badgesContainerRef = useRef(null);
 
     const backgroundColor = {
         backgroundColor: props.backgroundColor ? 'var(--background-color2)' : 'var(--background-color1)'
+    }
+
+    const handleMouseMove = (e) => {
+        const badgesContainer = badgesContainerRef.current;
+        const rect = badgesContainer.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        badgesContainer.style.setProperty('--x', `${x}px`);
+        badgesContainer.style.setProperty('--y', `${y}px`);
     }
 
     return (
@@ -16,7 +27,7 @@ function MySkillsSection(props) {
             <div className='my-skills-inner-conatiner'>
                 <h2>My Skills</h2>
                 <p>I can put your ideas and thus your wishes in the form of a unique solution that can help you achive your goals.</p>
-                <div className='badges-conatiner'>
+                <div className='badges-container' ref={badgesContainerRef} onMouseMove={handleMouseMove}>
                     {skills.map((skill, index) => (
                         <SkillBadge
                             key={index}
