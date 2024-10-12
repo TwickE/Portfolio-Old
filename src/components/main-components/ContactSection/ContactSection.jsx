dec54c87561a5be483f265b24f998dcfa5f8e998
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-10-12 18:06:15 +0100

    Continue to develop the ContactSection

--- src/components/main-components/ContactSection/ContactSection.jsx
+++ src/components/main-components/ContactSection/ContactSection.jsx
@@ -1,8 +1,13 @@
 import './ContactSection.css'
 import PropTypes from 'prop-types';
 import FilledButton from '../../sub-components/FilledButton/FilledButton';
+import { useRef } from 'react';
+import Swal from 'sweetalert2'
+import iconsFile from '../../../assets/icons.svg'
 
 function ContactSection(props) {
+    const formRef = useRef(null);
+
     const backgroundColor = {
         backgroundColor: props.backgroundColor ? 'var(--background-color2)' : 'var(--background-color1)'
     }
@@ -26,29 +31,56 @@ function ContactSection(props) {
         }).then((res) => res.json());
 
         if (res.success) {
-            console.log("Success", res);
+            Swal.fire({
+                title: "Success!",
+                text: "Message sent successfully!",
+                icon: "success",
+                color: "var(--text-color)",
+                background: "var(--accent-color)",
+                buttonsStyling: false,
+                customClass: {
+                    confirmButton: "filled-button"
+                },
+                confirmButtonText: "Ok"
+
+            });
+            formRef.current.reset();
         }
     };
 
     return (
         <section style={backgroundColor} className='contact-conatiner'>
+            <object type="image/svg+xml" data={iconsFile} style={{display: 'none'}}></object>
             <div className='contact-inner-container'>
-                <form onSubmit={onSubmit}>
+                <form onSubmit={onSubmit} ref={formRef}>
                     <h2>Let&apos;s Talk!</h2>
                     <p>I design and code beautifully simple things and i love what i do. Just simple like that!</p>
                     <div className='form-devide-inputs'>
-                        <input className='form-inputs' type="text" name='First Name' placeholder='First Name' />
-                        <input className='form-inputs' type="text" name='Last Name' placeholder='Last Name' />
+                        <input className='form-inputs' type="text" name='First Name' placeholder='First Name' required/>
+                        <input className='form-inputs' type="text" name='Last Name' placeholder='Last Name' required/>
                     </div>
                     <div className='form-devide-inputs'>
-                        <input className='form-inputs' type="email" name='Email Adress' placeholder='Email Adress' />
-                        <input className='form-inputs' type="tel" name='Phone Number' placeholder='Phone Number' />
+                        <input className='form-inputs' type="email" name='Email Adress' placeholder='Email Adress' required/>
+                        <input className='form-inputs' type="tel" name='Phone Number' placeholder='Phone Number' required/>
                     </div>
-                    <textarea className='form-textarea' name='Message' placeholder='Message'></textarea>
+                    <textarea className='form-textarea' name='Message' placeholder='Message' required></textarea>
                     <span className='form-button'>
                         <FilledButton text='Send Message' />
                     </span>
                 </form>
+                <div className='contact-badges-container'>
+                    <a className='contact-badge' href='mailto: fredericosilva2002@hotmail.com.com' target='_blank'>
+                        <span>
+                            <svg>
+                                <use href={`${iconsFile}#mail-icon`}></use>
+                            </svg>
+                        </span>
+                        <div className='contact-badge-text-container'>
+                            <h6>Email</h6>
+                            <h4>fredericosilva2002@hotmail.com</h4>
+                        </div>
+                    </a>
+                </div>
             </div>
         </section>
     )
