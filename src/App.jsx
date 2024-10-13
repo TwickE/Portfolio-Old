39fe1219876848589450b94e86ab9b4bb9df5279
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-10-13 17:22:04 +0100

    Added a feature that changes the title of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { useEffect } from 'react';
 import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
@@ -6,6 +7,27 @@ import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
 
 function App() {
+    const location = useLocation();
+
+    useEffect(() => {
+        const path = location.pathname;
+        let title = 'My Portfolio'; // Default title
+
+        if (path === '/') {
+            title = 'Home - My Portfolio';
+        } else if (path.startsWith('/about')) {
+            title = 'About - My Portfolio';
+        } else if (path.startsWith('/projects')) {
+            title = 'Projects - My Portfolio';
+        } else if (path.startsWith('/contact')) {
+            title = 'Contact - My Portfolio';
+        } else {
+            title = '404 Not Found - My Portfolio';
+        }
+
+        document.title = title;
+    }, [location]);
+
     return (
         <Routes>
             <Route path='/' element={<Home />} />
