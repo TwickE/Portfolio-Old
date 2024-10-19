e2f1efb7d300ab43b12025e276bbe40cb6c0a4a3
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-10-19 12:16:49 +0100

    Made the footer always be in the bottom of the website

--- src/components/main-components/Footer/Footer.jsx
+++ src/components/main-components/Footer/Footer.jsx
@@ -2,12 +2,34 @@ import './Footer.css'
 import iconsFile from '../../../assets/icons.svg'
 import { Link } from 'react-router-dom'
 import ReactImage from '../../../assets/skills-images/react-icon.webp'
+import { useEffect, useState } from 'react';
 
 function Footer() {
     const currentYear = new Date().getFullYear();
+    const [footerStyle, setFooterStyle] = useState({});
+
+    useEffect(() => {
+        const checkFooterPosition = () => {
+            const contentHeight = document.body.scrollHeight;
+            const viewportHeight = window.innerHeight;
+
+            if (contentHeight < viewportHeight) {
+                setFooterStyle({ position: 'fixed'});
+            } else {
+                setFooterStyle({ position: 'static' });
+            }
+        };
+
+        checkFooterPosition();
+        window.addEventListener('resize', checkFooterPosition);
+
+        return () => {
+            window.removeEventListener('resize', checkFooterPosition);
+        };
+    }, []);
 
     return (
-        <footer className='footer-container'>
+        <footer style={footerStyle} className='footer-container'>
             <div className='footer-inner-container'>
                 <Link className='footer-navlink' to='/'>
                     <svg className='footer-main-icon'>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/main-components/Navbar/Navbar"
 import BreadcrumbSection from "../components/main-components/BreadcrumbSection/BreadcrumbSection"
+import Footer from "../components/main-components/Footer/Footer"
 
 function About() {
     return (
         <>
             <Navbar activeLink="about" />
             <BreadcrumbSection location="About" />
+            <Footer />
         </>
     )
 }
