6a72e281df723fb5b31fb50a05a01adfb09d5112
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-10-20 19:03:12 +0100

    Added the LetsTalkSection main-component

--- src/components/main-components/BreadcrumbSection/BreadcrumbSection.jsx
+++ src/components/main-components/BreadcrumbSection/BreadcrumbSection.jsx
@@ -3,23 +3,45 @@ import PropTypes from 'prop-types';
 import backgroundImage from '../../../assets/breadcrumbBackground.webp'
 import { Link } from 'react-router-dom';
 import iconsFile from '../../../assets/icons.svg'
+import { useEffect, useRef, useState } from 'react';
 
 function BreadcrumbSection(props) {
     const breadcrumbBackgroundImage = {
         backgroundImage: `url(${backgroundImage})`
     }
+    const breadcrumbRef = useRef(null);
+    const [breadcrumbHeight, setBreadcrumbHeight] = useState(0);
+
+    const updateBreadcrumbHeight = () => {
+        if (breadcrumbRef.current) {
+            const { height } = breadcrumbRef.current.getBoundingClientRect();
+            setBreadcrumbHeight(height - 100);
+        }
+    };
+
+    useEffect(() => {
+        updateBreadcrumbHeight();
+        window.addEventListener('resize', updateBreadcrumbHeight);
+
+        return () => {
+            window.removeEventListener('resize', updateBreadcrumbHeight);
+        };
+    }, []);
 
     return (
-        <section style={breadcrumbBackgroundImage} className='breadcrumb-container'>
-            <h1>{props.location}</h1>
-            <div className='breadcrumb-path-container'>
-                <Link className='breadcrumb-link' to='/'>Home</Link>
-                <svg>
-                    <use href={`${iconsFile}#arrow-icon`}></use>
-                </svg>
-                <span>{props.location}</span>
-            </div>
-        </section>
+        <>
+            <section ref={breadcrumbRef} style={breadcrumbBackgroundImage} className='breadcrumb-container'>
+                <h1>{props.location}</h1>
+                <div className='breadcrumb-path-container'>
+                    <Link className='breadcrumb-link' to='/'>Home</Link>
+                    <svg>
+                        <use href={`${iconsFile}#arrow-icon`}></use>
+                    </svg>
+                    <span>{props.location}</span>
+                </div>
+            </section>
+            <span style={{ height: `${breadcrumbHeight}px`, display: 'block' }}></span>
+        </>
     )
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,18 @@
 import Navbar from "../components/main-components/Navbar/Navbar"
 import BreadcrumbSection from "../components/main-components/BreadcrumbSection/BreadcrumbSection"
 import Footer from "../components/main-components/Footer/Footer"
+import MySkillsSection from "../components/main-components/MySkillsSection/MySkillsSection"
+import MyResumeSection from "../components/main-components/MyResumeSection/MyResumeSection"
+import LetsTalkSection from "../components/main-components/LetsTalkSection/LetsTalkSection"
 
 function About() {
     return (
         <>
             <Navbar activeLink="about" />
             <BreadcrumbSection location="About" />
+            <MySkillsSection backgroundColor={true}/>
+            <MyResumeSection backgroundColor={false}/>
+            <LetsTalkSection backgroundColor={true}/>
             <Footer />
         </>
     )
