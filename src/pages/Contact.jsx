4e4b2425d479d9b50533a4b38658414d90a63c96
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-10-27 00:11:27 +0100

    Finished the Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,19 @@
 import Navbar from "../components/main-components/Navbar/Navbar"
+import BreadcrumbSection from "../components/main-components/BreadcrumbSection/BreadcrumbSection"
+import ScrollToTop from "../hooks/ScrollToTop"
+import ContactSection from "../components/main-components/ContactSection/ContactSection"
+import GoUp from "../components/sub-components/GoUp/GoUp"
+import Footer from "../components/main-components/Footer/Footer"
 
 function Contact() {
     return (
         <>
+            <ScrollToTop />
             <Navbar activeLink="contact" />
-            <h1>Contact</h1>
+            <BreadcrumbSection location="Contact" />
+            <ContactSection backgroundColor={true} />
+            <GoUp />
+            <Footer />
         </>
     )
 }
