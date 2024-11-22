a8fde6b9ebfbac98313ec0ec690eda94bcb8c09c
TwickE <118013263+TwickE@users.noreply.github.com>
2024-11-22 15:47:41 +0000

    Started working with a new animation package

--- src/components/main-components/NotFoundSection/NotFoundSection.jsx
+++ src/components/main-components/NotFoundSection/NotFoundSection.jsx
@@ -1,6 +1,7 @@
 import './NotFoundSection.css'
 import { useRef } from 'react'
 import { useNavigate } from 'react-router-dom';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const NotFoundSection = () => {
     const buttonRef = useRef(null)
@@ -12,11 +13,11 @@ const NotFoundSection = () => {
 
     return (
         <section className='not-found-section'>
-            <div className='container-not-found-info'>
+            <ScrollAnimation animateIn='zoomIn' className='container-not-found-info'>
                 <h1>404</h1>
                 <h2>It seems that you are lost</h2>
                 <button className='filled-button' ref={buttonRef} onClick={handleClick}>Go Home</button>
-            </div>
+            </ScrollAnimation>
         </section>
     )
 }
